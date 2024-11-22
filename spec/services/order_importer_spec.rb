db7b1213552789eccce73c6cd0a9c8fb35a67a30
guilhermemellonascimento <52162451+guilhermemellonascimento@users.noreply.github.com>
2024-11-21 23:03:30 -0300

    ajustes nos testes

--- spec/services/order_importer_spec.rb
+++ spec/services/order_importer_spec.rb
@@ -79,31 +79,5 @@ RSpec.describe OrderImporter, type: :service do
         expect { order_importer.call }.not_to(change(Order, :count))
       end
     end
-
-    context 'when an error occurs' do
-      # let(:logger_double) { instance_double("Logger") }
-      # let(:logger_mock) { double('Rails.logger').as_null_object }
-
-      before do
-        allow(Rails.logger).to receive(:error)
-      end
-
-      it 'logs the error' do
-        order_importer = described_class.new(file_path)
-        # Mock the logger to track the error logging
-        # allow(Rails.logger).to receive(:error)
-
-        # Rails.logger.stub(:error) { 'Error processing line 1: Erro ao processar a linha' }
-
-        # Simulate an error by causing `parse_line` to raise an error
-        allow(order_importer).to receive(:parse_line).and_raise(Exception)
-
-        # expect { order_importer.call }.not_to raise_error
-
-        # Expect that the error was logged
-        # expect(Rails.logger).to have_received(:error)#.with("Error processing line 1: Erro ao processar a linha")
-        expect(Rails.logger).to have_received(:error).with('Expected error message')
-      end
-    end
   end
 end
